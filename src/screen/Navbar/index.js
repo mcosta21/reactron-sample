5650a5fa1b2e69fa9d9a0b315dcdd6291196ceba
Marcio Costa <marcioc424@gmail.com>
2020-07-27 15:00:27 -0300

    First commit

--- src/screen/Navbar/index.js
+++ src/screen/Navbar/index.js
@@ -3,11 +3,6 @@ import {Container} from './styles';
 import OptionMenu from '../../components/OptionMenu'
 import Logo from '../../media/logo.png';
 
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { withStyles } from '@material-ui/core/styles';
-
 function Navbar() {
 
     return (
